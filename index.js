e2c330030fce495939add4803986d7c33ed8f3a5
Luis Maestre <[luisaugustomaestre@gmail.com]>
2021-06-17 17:42:11 -0300

    agregue archivo js

---
+++ index.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu");
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle("opacity");
+})
